0519a705dc7f27bdc5920a99f953bc9375fd757b
rnjstjdgh <gshgsh0831@daum.net>
2018-08-03 12:51:34 +0900

    haha

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,22 @@ from bs4 import BeautifulSoup
 import requests as rq
 from time import sleep
 
+def crol_RealTimeContents():
+    url_main_p = 'https://www.daum.net/'
+    res_main_p = rq.get(url_main_p)
+    soup_main = BeautifulSoup(res_main_p.text, 'html.parser')
+    # print(soup_main)
+    dic = {}  # 순위와 내용을 메핑시킬 dict선언 {'rank':'content'}
+    result = soup_main.find('div', {'class': 'hotissue_mini'})
+    # print(result)
+    contents = result.find_all('a', {'class': 'link_issue'})
+    for i in range(1, 11):
+        dic[i] = contents[i - 1].text
+    return dic
 
+list=[]
+for i in crol_RealTimeContents().values():
+    list.append(i)
 
 
 
